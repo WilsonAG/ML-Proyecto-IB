714426b3dbf411f0009c807bf0e8a51477902a0c
Gabriel Cacuango <gabrielcacuango07@gmail.com>
2020-06-10 17:54:03 -0500

    data

--- lib/words.py
+++ lib/words.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import json 
+import emoji
 
 def good_words():
     file = open('./data/diccionary/buenas.txt', 'r')
@@ -15,11 +16,19 @@ def bad_words():
     return items
 
 
-good = good_words()
-bad = bad_words()
-for i in good:
-    print(i)
+#good = good_words()
+#bad = bad_words()
+#for i in good:
+#    print(i)
 #print('------- buena spalabras ')
 # print(good)
 #print('------- buena spalabras ')
 # print(bad)
+
+good_emoji=open('data\emojis\good_emojis.json','r')
+good_emoji =json.load(good_emoji)
+
+for i in good_emoji:
+    print(emoji.emojize(i,use_aliases=True))
+
+
