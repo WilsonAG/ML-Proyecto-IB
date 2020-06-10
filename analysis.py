22eda585af572d38534016ad51531ca402b212b6
rromom@est.ups.edu.ec <rromom@est.ups.edu.ec>
2020-06-10 16:58:43 -0500

    limpieza diccionario palabras buenas y malas

--- analysis.py
+++ analysis.py
@@ -1,8 +1,24 @@
 import lib.nlp as nlp
 import pandas as pd
 
+def get_dictionary(doc):
+    f=open(doc)
+    dictionary=[]
+    for i in f:
+        dictionary.append(str(i))
+    dictionary=list(map(nlp.clean,dictionary))
+    dictionary=list(map(lambda x: x.split(),dictionary))
+    dictionary=list(map(nlp.clean_stemmer,dictionary))
+    dictionary=list(map(lambda x: " ".join(x),dictionary))
+    return dictionary
+
+
+
 if __name__ == "__main__":
     data = pd.read_excel('./data.xlsx', encoding='utf-8')
+    good='./data/new/buenas.txt'
+    bad='./data/new/mala.txt'
+    
     index = 0
     docs=list(map(lambda x: str(x).strip(),data['tweets']))
     docs=list(map(nlp.clean,docs))
@@ -10,5 +26,9 @@ if __name__ == "__main__":
     docs=list(map(nlp.clean_stop_words,docs))
     docs=list(map(nlp.clean_stemmer,docs))
     fii=nlp.get_fii(docs)
-    for i in fii:
-        print(i[0])
+    
+    good=get_dictionary(good)
+    bad=get_dictionary(bad)
+    print(bad)
+    print(len(bad))
+
