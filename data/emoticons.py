3a1a87380c5763ca8d2cf0adf1742222c456b68f
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-06-10 16:58:33 -0500

    emojis init

---
+++ data/emoticons.py
@@ -0,0 +1,6 @@
+import emoji
+import sys
+
+# emoji.emojkize(use_aliases=True)
+
+print(sys.version_info[0])
--- lib/words.py
+++ lib/words.py
@@ -1,17 +1,25 @@
 import pandas as pd
+
+
 def good_words():
-    file = open('../data/diccionary/buenas.txt', 'r') 
-    text = file.read() 
+    file = open('./data/diccionary/buenas.txt', 'r')
+    text = file.read()
     items = text.split(",")
     return items
+
+
 def bad_words():
-    file = open('../data/diccionary/malas.txt', 'r') 
-    text = file.read() 
+    file = open('./data/diccionary/malas.txt', 'r')
+    text = file.read()
     items = text.split(",")
     return items
-good=good_words()
-bad= bad_words()
+
+
+good = good_words()
+bad = bad_words()
+for i in good:
+    print(i)
 #print('------- buena spalabras ')
-#print(good)
+# print(good)
 #print('------- buena spalabras ')
-#print(bad)
+# print(bad)
