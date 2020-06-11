f5c521f23c1b1ad92eac37340cd8ad3f6a57c9cb
rromom@est.ups.edu.ec <rromom@est.ups.edu.ec>
2020-06-10 19:39:34 -0500

    diccionarios buenos y malos completos con emoticones

--- analysis.py
+++ analysis.py
@@ -1,8 +1,10 @@
 import lib.nlp as nlp
 import pandas as pd
+import lib.emoticons as emo
+import emoji
 
 def get_dictionary(doc):
-    f=open(doc)
+    f=open(doc,encoding='utf-8')
     dictionary=[]
     for i in f:
         dictionary.append(i)
@@ -10,7 +12,7 @@ def get_dictionary(doc):
     dictionary=list(map(lambda x: x.split(),dictionary))
     dictionary=list(map(nlp.clean_stemmer,dictionary))
     dictionary=list(map(lambda x: " ".join(x),dictionary))
-    return set(dictionary)
+    return list(set(dictionary))
 
 def get_jaccard(query, document):
     interseccion = len(query.intersection(document))
@@ -25,11 +27,13 @@ if __name__ == "__main__":
     data = pd.read_excel('./data.xlsx', encoding='utf-8')
     good='./data/new/buenas.txt'
     bad='./data/new/mala.txt'
+    good_emoticon='data/emojis/good_emojis.json'
+    bad_emoticon='data/emojis/bad_emojis.json'
 
     index = 0
     docs=list(map(lambda x: str(x).strip(),data['tweets']))
     docs=list(map(nlp.clean,docs))
-    print(docs[806])
+    #print(docs[806])
     docs=list(map(lambda x: x.split(),docs))
     docs=list(map(nlp.clean_stop_words,docs))
     docs=list(map(nlp.clean_stemmer,docs))
@@ -37,12 +41,18 @@ if __name__ == "__main__":
 
     good=get_dictionary(good)
     bad=get_dictionary(bad)
+    good_emoticon=emo.get_emojis(good_emoticon)
+    bad_emoticon=emo.get_emojis(bad_emoticon)
+    good+=good_emoticon
+    bad+=bad_emoticon
+    print(len(good))
+    print(len(bad))
     
-    doc=docs[806]
-    print(doc)
     
-    more=get_jaccard(set(good),set(doc))
-    low=get_jaccard(set(bad),set(doc))
-    print(more," ",low)
+    # doc=docs[806]
+    # print(doc)
+    
+    # more=get_jaccard(set(good),set(doc))
+    # low=get_jaccard(set(bad),set(doc))
 
 
---
+++ lib/emoticons.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import json 
+import emoji
+
+def good_words():
+    file = open('./data/diccionary/buenas.txt', 'r')
+    text = file.read()
+    items = text.split(",")
+    return items
+
+
+def bad_words():
+    file = open('./data/diccionary/malas.txt', 'r')
+    text = file.read()
+    items = text.split(",")
+    return items
+
+def get_emojis(doc):
+    emoticon=open(doc,'r')
+    emoticon=json.load(emoticon)
+    emotico=[]
+    for i in emoticon:
+        emotico.append(emoji.emojize(i,use_aliases=True))
+    return emotico
+
+
+
+# good_emoji=open('data/emojis/good_emojis.json','r')
+# bad_emoji=open('data/emojis/bad_emojis.json','r')
+# good_emoji =json.load(good_emoji)
+# bad_emoji =json.load(bad_emoji)
+
+# for i in good_emoji:
+#     print(emoji.emojize(i,use_aliases=True))
+
+
+# '''
+# devolver buenos y malos pero los emojis echos iconos con el emojize
+# '''
+
