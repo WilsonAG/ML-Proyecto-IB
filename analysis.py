5a126a42fcacf4645f34b4e6fd0418d7f2405299
rromom@est.ups.edu.ec <rromom@est.ups.edu.ec>
2020-06-11 20:36:32 -0500

    nlp analys.py

--- analysis.py
+++ analysis.py
@@ -29,26 +29,26 @@ if __name__ == "__main__":
     docs = list(map(lambda x: x.split(), docs))
     docs = list(map(nlp.clean_stop_words, docs))
     docs = list(map(nlp.clean_stemmer, docs))
-    # # fii=nlp.get_fii(docs)
+    # fii=nlp.get_fii(docs)
 
-    good = get_dictionary(good)
-    bad = get_dictionary(bad)
+    good = nlp.get_dictionary(good)
+    bad = nlp.get_dictionary(bad)
     good_emoticon = emo.get_emojis(good_emoticon)
     bad_emoticon = emo.get_emojis(bad_emoticon)
     good += good_emoticon
     bad += bad_emoticon
 
-    # print(len(good))
-    # print(len(bad))
     print(len(bad)-len(good))
 
     doc = docs[num]
     print(doc)
 
-    more = get_jaccard(set(good), set(doc))
-    low = get_jaccard(set(bad), set(doc))
+    more = nlp.get_jaccard(set(good), set(doc))
+    low = nlp.get_jaccard(set(bad), set(doc))
     etiqueta = get_etiquetado(more, low)
     print(etiqueta)
 
     good_fii = nlp.get_fii(docs, good)
     bad_fii = nlp.get_fii(docs, bad)
+
+    
