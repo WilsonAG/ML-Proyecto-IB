deb3294842350519a65b583e5b6c6fac255cbc14
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-06-14 11:51:19 -0500

    regresion modularized to lib folder

---
+++ regresion_log.py
@@ -0,0 +1,8 @@
+import lib.regresion as reg
+import lib.nlp as nlp
+
+
+if __name__ == "__main__":
+    train, test = reg.load_data('data/tweets/tweets.csv')
+    model = reg.eval(train, test)
+    reg.test_model(model, train, test)
